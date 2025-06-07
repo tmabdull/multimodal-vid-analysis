57d0e083744fa07fe460b7bf7c41bab7fea6f6f5
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2025-06-07 10:48:21 -0700

    Try / catch for visual api

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from .utils.visual_pipeline_utils import create_vid_embeddings, visual_query
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -12,14 +12,19 @@ class QueryRequest(BaseModel):
 
 @app.post("/create_vid_embeddings")
 def create_vid_embeddings_api(req: VideoRequest):
-    # Call your Python function
-    collection_name_with_vid_embeddings = create_vid_embeddings(req.youtube_url, loaded=True)
-    return {
-        "status": "processed",
-        "chroma_collection_name": collection_name_with_vid_embeddings
-    }
+    try:
+        collection_name_with_vid_embeddings = create_vid_embeddings(req.youtube_url, loaded=True)
+        return {
+            "status": "processed",
+            "chroma_collection_name": collection_name_with_vid_embeddings
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/visual_query")
 def visual_query_api(req: QueryRequest):
-    matches = visual_query(req.query_text)
-    return {"matches": matches}
+    try:
+        matches = visual_query(req.query_text)
+        return {"matches": matches}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
